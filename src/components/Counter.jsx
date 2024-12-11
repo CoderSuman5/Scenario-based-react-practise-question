7320da756527183694f50af4fd6b17452cd6251b
suman biswas <satyajeetbishwash162087@gmail.com>
2024-12-11 12:55:13 +0530

    added function for reseting count

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -15,12 +15,18 @@ const Counter = () => {
         }
         console.log(count);
     }
+
+    let handleReset = () => {
+        setCount(0)
+        console.log(count);
+    }
     
   return (
     <>Counter
          <h2>count is: {count}</h2>
          <button onClick={handleIncrease}>increase</button>
          <button onClick={handleDecrease}>decrease</button>
+         <button onClick={handleReset}>reset</button>
     </>
   )
 }
